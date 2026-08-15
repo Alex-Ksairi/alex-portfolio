024c809e8d65fbdbc8c0b51c8f2588a0f0d97fda
Alexander Ksairi <alexanderksairi@Alexs-MacBook-Pro.local>
2026-08-15 13:22:30 +0200

    herd configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+
+        hmr: {
+            host: '192.168.178.162',
+            port: 5173,
+        },
+    },
+
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
